c62e7bb9d259f82d7f377bab29ff76a34a0274fc
othomation <contact@otho.bike>
2023-12-09 23:28:03 +0100

    Add solution 1

---
+++ solutions/1/index.ts
@@ -0,0 +1,31 @@
+import Solution, { SolutionSolverType } from "../../solution";
+
+const name = "Day 1: Trebuchet?!";
+
+const solver: SolutionSolverType = (input) => {
+  const lines = input.split(`\r\n`);
+  const numbersOnly = lines.map((line) => {
+    let firstDigit = "";
+    let lastDigit = "";
+
+    for (const char of line.split("")) {
+      if (!isNaN(parseInt(char))) {
+        firstDigit = char;
+        break;
+      }
+    }
+
+    for (const char of line.split("").reverse()) {
+      if (!isNaN(parseInt(char))) {
+        lastDigit = char;
+        break;
+      }
+    }
+    return firstDigit + lastDigit;
+  });
+
+  const sumOfDigits = numbersOnly.reduce((a, b) => a + parseInt(b), 0);
+  return sumOfDigits.toString();
+};
+
+export default new Solution(1, name, solver);
